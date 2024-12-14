9760f52577e472d014ddb7b7129a651fba3f2a3f
idir <idirxdziri@gmail.com>
2024-12-14 11:48:25 +0100

    add the endpoints

--- lib/core/constant/endpoints.dart
+++ lib/core/constant/endpoints.dart
@@ -1,4 +1,31 @@
 /// This class [Endpoints] contains all the endpoints of the API
 class Endpoints {
-  static const String baseUrl = '';
+  static const String baseUrl = 'http://localhost:3000';
+
+//Auth
+  //post
+  static const String login = '/auth/login';
+  //post
+  static const String register = '/auth/register';
+  //post
+  static const String logout = '/auth/logout';
+//User
+  //put
+  static const String userUpdate = '/user/{id}';
+  //delete
+  static const String userDelete = '/user/delete';
+  //get
+  static const String getUser = '/user/{id}';
+
+  //Projects
+  //get
+  static const String getProjects = '/projects/dashboard';
+  //post
+  static const String createProject = '/projects';
+  //get
+  static const String getProjectDetails = '/projects/{id}';
+  //put
+  static const String updateProject = '/projects/{id}';
+  //delete
+  static const String deleteProject = '/projects/{id}';
 }
